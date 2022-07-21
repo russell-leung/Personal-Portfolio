c17d9a5eee25b9c2bdbf760657371f81bf22985b
RussellLeung <>
2022-07-21 17:45:03 -0400

    Russell Leung - Personal Portfolio Website

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
+// Dropdown js - mobile nav
+let mobile_nav_dropdown = document.querySelector(".mobile-dropbtn");
+mobile_nav_dropdown.addEventListener("click", toggle_nav.bind(null, ".mobile_dropdown_content"));
+document.querySelector(".mobile_dropdown_content").style.display = "none";
 
-// Dropdown js
-function dropdownToggle() {
-  document.getElementById("email").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function toggle_nav(x) {
+  if (document.querySelector(x).style.display == "none") {
+    document.querySelector(x).style.display = "flex";
+  } else {
+    document.querySelector(x).style.display = "none";
   }
 }
+
+
+// Change EC title to NECC on hover --> What is wrong with this?? Throws an error that messes up form js
+
+// let title = document.getElementById("ECtitle");
+// title.addEventListener("mouseover", mouseOver);
+// title.addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  title.innerHTML = "@ Northern Essex Community College";
+}
+
+function mouseOut() {
+  title.innerHTML = "Early College / Dual Enrollment";
+}
+
+// Contact Form Submit
+window.addEventListener("load", () => {
+  const form = document.getElementById('contact-me-form');
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+      .then(() => {
+
+        //change this to wipe previous form
+        form.reset()
+        alert("Form Sucessfully Submitted!");
+      })
+  });
+});
 
